2b0c063062c6af21807f9603e16eb756305b1bcb
Jonathan Woahn <jonathan@catalant.co>
2019-06-04 11:04:26 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+import { Todo } from './app.module';
+import { DynamicFacadeService, DynamicFacade } from 'dynamic-ngrx';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ez-ngrx';
+  @ViewChild('todoInput') todoInput: ElementRef;
+
+  todos$: Observable<Todo[]>;
+
+  private todoFacade: DynamicFacade<Todo>;
+  constructor(
+    private dynamicFacadeService: DynamicFacadeService,
+    private store: Store<any>,
+  ) {
+    this.todoFacade = this.dynamicFacadeService.getFacade<Todo>('Todo');
+    this.todos$ = this.todoFacade.entities$;
+  }
+
+  addTodo(event: KeyboardEvent): void {
+    if (event.keyCode !== 13) { return; }
+    const todo: Todo = {
+      id: uuid(),
+      text: this.todoInput.nativeElement.value,
+    };
+    this.store.dispatch(this.todoFacade.actions.addOne(todo));
+    this.todoInput.nativeElement.value = '';
+  }
+
+  removeTodo(todo: Todo): void {
+    this.store.dispatch(this.todoFacade.actions.removeOne(todo.id));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule, MatInputModule, MatListModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { DynamicNgrxModule, DynamicStoreConfig } from 'dynamic-ngrx';
 
+const config: DynamicStoreConfig = {
+  entities: [{ entity: 'Todo' }],
+  providers: [],
+  enableOfflineSync: true,
+};
+
+export interface Todo {
+  id: string;
+  text: string;
+}
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    DynamicNgrxModule.forRoot(config),
   ],
   providers: [],
   bootstrap: [AppComponent]
